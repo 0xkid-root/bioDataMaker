528684225da7606a8fb073c59e8f1974d73f8577
0xkid-root <prohackers698@gmail.com>
2025-12-25 07:16:56 +0530

    crousal include

--- src/components/BiodataCreator.tsx
+++ src/components/BiodataCreator.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import {  useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { Shield } from 'lucide-react';
 import { useBiodata } from '../context/BiodataContext';
